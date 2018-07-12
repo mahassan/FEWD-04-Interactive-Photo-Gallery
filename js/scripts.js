6e84df2c38b322ebe61be66bae224f72e85cf880
Ammar Hassan <ammar@ammars-imac.home>
2018-07-12 18:49:05 +0100

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
     //get reference for searchbox
     const searchBox = $("#search");
     const images = $('.main a img');
-    const captions = $(images).parent().parent();
-    console.log(captions);
+    const anchors = $(images).parent().parent();
     /*
     As project require us to filter real time
     we need to use event that trigger on every
@@ -16,13 +15,13 @@ $(document).ready(function(){
             //if searchbox value is equal/matching
                 if(
                     images[i].alt.includes(lowerCase) || 
-                    captions[i].dataset.caption.includes(lowerCase)
+                    anchors[i].dataset.caption.includes(lowerCase)
                  ){
                         //then display that images
-                        $(captions[i]).show();
+                        $(anchors[i]).show();
                  }else{
                         //else hide 
-                        $(captions[i]).hide();
+                        $(anchors[i]).hide();
                     }
             }
             
