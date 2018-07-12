0cfa7014e70e04fdc3bc35203f9d38fc7ed31a8f
Ammar Hassan <ammar@ammars-imac.home>
2018-07-12 17:03:01 +0100

    Removed lightbox dir

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     const searchBox = $("#search");
     const images = $('.main a img');
     const captions = $(images).parent().parent();
-
+    console.log(captions);
     /*
     As project require us to filter real time
     we need to use event that trigger on every
@@ -15,8 +15,8 @@ $(document).ready(function(){
             //if searchbox value is equal/matching
                 if(images[i].alt.includes(searchBox.val().toLowerCase()) 
                 || images[i].alt.includes(searchBox.val().toUpperCase()) 
-                || captions[i].dataset.title.includes(searchBox.val().toLowerCase())
-                || captions[i].dataset.title.includes(searchBox.val().toUpperCase())){
+                || captions[i].dataset.caption.includes(searchBox.val().toLowerCase())
+                || captions[i].dataset.caption.includes(searchBox.val().toUpperCase())){
                     //then display that images
                     $(images[i]).show();
                 }else{
