d68cbef60b6efe0fc41366814ba2aa57cfe505e3
Ammar Hassan <ammar@ammars-imac.home>
2018-07-12 18:45:26 +0100

    Removed related files to lightbox2

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,19 @@ $(document).ready(function(){
     searchBox.on('keydown', function(){
         //loop over images
             for(let i = 0; i <images.length;i++){
+                let lowerCase = searchBox.val().toLowerCase()
             //if searchbox value is equal/matching
-                if(images[i].alt.includes(searchBox.val().toLowerCase()) 
-                || images[i].alt.includes(searchBox.val().toUpperCase()) 
-                || captions[i].dataset.caption.includes(searchBox.val().toLowerCase())
-                || captions[i].dataset.caption.includes(searchBox.val().toUpperCase())){
-                    //then display that images
-                    $(images[i]).show();
-                }else{
-                    //else hide 
-                    $(images[i]).hide();
-                }
+                if(
+                    images[i].alt.includes(lowerCase) || 
+                    captions[i].dataset.caption.includes(lowerCase)
+                 ){
+                        //then display that images
+                        $(captions[i]).show();
+                 }else{
+                        //else hide 
+                        $(captions[i]).hide();
+                    }
             }
+            
     })
 })
